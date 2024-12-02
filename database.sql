277ff53b93b8ea881ddf71a47a3887eaf5f4c533
R1UK0 <ryuko.dvp@gmail.com>
2024-12-02 18:08:38 +0100

    Delete database.sql

--- database.sql
+++
@@ -1,9 +0,0 @@
-CREATE DATABASE ryuko;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) UNIQUE NOT NULL,
-    pseudo VARCHAR(50) UNIQUE NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    profile_pic VARCHAR(255) DEFAULT 'default-profile-pic.jpg'
-);
