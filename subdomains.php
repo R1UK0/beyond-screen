bd8ca46b9e9d8a26bb2b72ecc928ad4b0cfe080c
R1UK0 <ryuko.dvp@gmail.com>
2024-12-03 08:39:47 +0100

    Delete subdomains.php

--- subdomains.php
+++
@@ -1,6 +0,0 @@
-$user_subdomain = strtolower($username) . ".yourhub.local";
-$subdomain_path = "/path/to/project-root/subdomains/" . strtolower($username);
-
-if (!file_exists($subdomain_path)) {
-    mkdir($subdomain_path, 0777, true);
-}
