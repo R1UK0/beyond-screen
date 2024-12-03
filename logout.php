3eb34726ca5b8d72943ed0f97313f2e94192c14e
R1UK0 <ryuko.dvp@gmail.com>
2024-12-03 08:30:27 +0100

    Delete logout.php

--- logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();  // Démarre la session
-session_destroy();  // Détruit toutes les variables de session
-header("Location: index.html");  // Redirige l'utilisateur vers la page d'accueil
-exit;
-?>
